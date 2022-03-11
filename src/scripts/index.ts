09c4a2610ab91e51f861b83eaa06399bc01cb0bb
fabrizzio-gz <fabrizzio.gz@gmail.com>
2022-03-11 11:48:09 -0400

    chore(map): move map script to own folder

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,3 +1,3 @@
 import "../styles/globals.css";
 import "../styles/navbar.css";
-import "./map";
+import "./map/map";
--- src/scripts/map/map.ts
+++ src/scripts/map/map.ts
@@ -1,7 +1,7 @@
 import L, { LatLngBounds } from "leaflet";
 
-import DummyMap from "../assets/dummy-map.svg";
-import "../styles/map.css";
+import DummyMap from "../../assets/dummy-map.svg";
+import "../../styles/map.css";
 
 const map = L.map("map", {
   minZoom: -5,
