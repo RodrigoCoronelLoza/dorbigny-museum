ecf6d7432c71917e9775d8fc4082c134e0ed2e15
RodrigoC <drigo31416@gmail.com>
2022-03-15 22:45:37 -0400

    feat: add subsections

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,6 +1,7 @@
 import "../styles/footer.css";
 import "../styles/globals.css";
 import "../styles/navbar.css";
+import "../styles/subsections.css";
 import "./map/map";
 import { addModalEventListeners } from "./navbar";
 
