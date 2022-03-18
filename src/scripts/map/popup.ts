6264fa1f7837089e38cf353d0d1366b019be36ef
fabrizzio-gz <fabrizzio.gz@gmail.com>
2022-03-18 11:35:28 -0400

    refactor(map): create map popup using html elements
    Required to add click event listener

--- src/scripts/map/popup.ts
+++ src/scripts/map/popup.ts
@@ -11,12 +11,24 @@ export const makePopup = (
   content: string,
   refId: string
 ): L.Popup => {
-  const popupContent = `
-  <div class="museum-map-popup-content">
-    <div class="popup-title"><h4>${title}</h4></div>
-    <div class="popup-content">${content}
-      <button id="${refId}" class="popup-button">ver más ></button>
-    </div>
-  </div>`;
-  return L.popup(popupOptions).setContent(popupContent);
+  const titleElem = document.createElement("h4");
+  titleElem.textContent = title;
+  const popupTitle = document.createElement("div");
+  popupTitle.classList.add("popup-title"), popupTitle.appendChild(titleElem);
+
+  const popupButton = document.createElement("button");
+  popupButton.textContent = "ver más >";
+  popupButton.classList.add("popup-button");
+  popupButton.id = refId;
+  const popupContent = document.createElement("div");
+  popupContent.classList.add("popup-content");
+  popupContent.textContent = content;
+  popupContent.appendChild(popupButton);
+
+  const container = document.createElement("div");
+  container.classList.add("museum-map-popup-content");
+  container.appendChild(popupTitle);
+  container.appendChild(popupContent);
+
+  return L.popup(popupOptions).setContent(container);
 };
