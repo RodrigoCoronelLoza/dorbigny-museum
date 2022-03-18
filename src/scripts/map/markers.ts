47cf3eab8724c19def757649f23f2ff6f7cf0795
fabrizzio-gz <fabrizzio.gz@gmail.com>
2022-03-18 09:25:03 -0400

    feat(map): add all section markers

--- src/scripts/map/markers.ts
+++ src/scripts/map/markers.ts
@@ -6,6 +6,14 @@ import { makePopup } from "./popup";
 // const lorem = "Culpa aut aut natus quia hic esse quidem. Error saepe tempore deleniti deserunt porro ut. Dolorem tenetur consequatur sed ea molestiae quo suscipit architecto. ";
 const treesText =
   "El Museo está ubicado en un terreno de casi 10’000 m2. Alberga más de un centenar de árboles de más de veinte especies diferentes.";
+const frogsText =
+  "El Centro K’ayra se dedica a la conservación de especies de anfibios amenazadas de extinción. Su habitante más famoso es #RomeotheFrog, ¡hasta hace poco considerado el último de su especie!";
+const birdsText =
+  "Al visitar el Museo, ¡te sorprenderá el canto de los pájaros! Muchas aves anidan en los árboles de los predios del museo o lo visitan durante sus rutas migratorias.";
+const batsText =
+  "Durante las noches, varios murciélagos sobrevuelan los predios del Museo. Estos vigilantes nocturnos se alimentan de los muchos insectos de la zona.";
+const museumText =
+  "Las salas de exposición se encuentran en una antigua y magnífica casona. Aquí encontrarás fósiles y ejemplares de aves, mamíferos, peces e insectos de Bolivia.";
 
 const addMarker = (map: L.Map, x: number, y: number, popup: L.Popup) =>
   L.marker([x, y], { icon: svgIcon }).addTo(map).bindPopup(popup);
@@ -15,6 +23,30 @@ const addTreeMarker = (map: L.Map, x: number, y: number) => {
   addMarker(map, x, y, popup);
 };
 
+const addFrogMarker = (map: L.Map, x: number, y: number) => {
+  const popup = makePopup("anfibios", frogsText, "frogs-section");
+  addMarker(map, x, y, popup);
+};
+
+const addBirdMarker = (map: L.Map, x: number, y: number) => {
+  const popup = makePopup("aves", birdsText, "birds-section");
+  addMarker(map, x, y, popup);
+};
+
+const addBatMarker = (map: L.Map, x: number, y: number) => {
+  const popup = makePopup("murciélagos", batsText, "bats-section");
+  addMarker(map, x, y, popup);
+};
+
+const addMuseumMarker = (map: L.Map, x: number, y: number) => {
+  const popup = makePopup("museo", museumText, "museum-section");
+  addMarker(map, x, y, popup);
+};
+
 export const addMapMarkers = (map: L.Map) => {
   addTreeMarker(map, 100, 100);
+  addFrogMarker(map, 100, 150);
+  addBirdMarker(map, 100, 200);
+  addBatMarker(map, 100, 250);
+  addMuseumMarker(map, 100, 50);
 };
