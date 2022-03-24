fd7e8625561aca90ff9564dd74095d57b34dae2d
fabrizzio-gz <fabrizzio.gz@gmail.com>
2022-03-24 16:21:19 -0400

    feat(map): add pan map bounds

--- src/scripts/map/map.ts
+++ src/scripts/map/map.ts
@@ -4,19 +4,25 @@ import Map from "../../assets/mapa-dorbigny-min.png";
 import "../../styles/map.css";
 import { addMapMarkers } from "./markers";
 
+const imgUrl = Map;
+const imageBounds: LatLngBounds = L.latLngBounds(
+  L.latLng(0, 0),
+  L.latLng(500, 800)
+);
+
+const mapPanBounds: LatLngBounds = L.latLngBounds(
+  L.latLng(-300, -300),
+  L.latLng(800, 1200)
+);
+
 const map = L.map("map", {
   minZoom: -2,
   crs: L.CRS.Simple,
   center: [250, 300],
   zoom: -1,
   maxZoom: 1,
+  maxBounds: mapPanBounds,
 });
-
-const imgUrl = Map;
-const imageBounds: LatLngBounds = L.latLngBounds(
-  L.latLng(0, 0),
-  L.latLng(500, 800)
-);
 
 L.imageOverlay(imgUrl, imageBounds, {
   attribution:
