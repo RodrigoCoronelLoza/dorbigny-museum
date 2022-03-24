83ffe8097f503f6f9a601ba935dad7cb161c5390
fabrizzio-gz <fabrizzio.gz@gmail.com>
2022-03-23 23:55:16 -0400

    feat(map): add real map image overlay

--- src/scripts/map/map.ts
+++ src/scripts/map/map.ts
@@ -1,21 +1,26 @@
 import L, { LatLngBounds } from "leaflet";
 
-import DummyMap from "../../assets/dummy-map.svg";
+import Map from "../../assets/mapa-dorbigny-min.png";
 import "../../styles/map.css";
 import { addMapMarkers } from "./markers";
 
 const map = L.map("map", {
-  minZoom: -5,
+  minZoom: -2,
   crs: L.CRS.Simple,
+  center: [250, 300],
+  zoom: -1,
+  maxZoom: 1,
 });
 
-const imgUrl = DummyMap;
+const imgUrl = Map;
 const imageBounds: LatLngBounds = L.latLngBounds(
   L.latLng(0, 0),
-  L.latLng(210, 297)
+  L.latLng(500, 800)
 );
 
-map.fitBounds(imageBounds);
-L.imageOverlay(imgUrl, imageBounds).addTo(map);
+L.imageOverlay(imgUrl, imageBounds, {
+  attribution:
+    'Ilustración <a href="https://www.instagram.com/che_churka/">Che Churka</a>',
+}).addTo(map);
 
 addMapMarkers(map);
